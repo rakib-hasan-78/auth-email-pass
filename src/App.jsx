5c87e55621857e2bf7872e6b60da885eda5df72f
rakib hasan <rakibwebdev78@gmail.com>
2026-02-08 17:47:27 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 
+      className='text-9xl text-red-400 font-bold'
+      >
+      Authentication using Email & password!
+      </h1>
+    </>
+  )
+}
+
+export default App
